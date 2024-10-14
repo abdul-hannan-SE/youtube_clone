e334f2f603511f4a84d0448d2c9ad1add2603147
abdul-SE-Flutter <114971771+abdul-hannan-SE@users.noreply.github.com>
2024-10-14 12:12:37 -0700

    added comment model

---
+++ models/comment.models.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const commentSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    video: {
+      type: Schema.Types.ObjectId,
+      ref: "Video",
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+commentSchema.plugin(mongooseAggregatePaginate);
+
+module.exports = mongoose.model("Comment", commentSchema);
